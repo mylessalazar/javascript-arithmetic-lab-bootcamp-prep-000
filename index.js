1dc54ed1e1bdaacccaf64a44452bc3bf06be0d4a
Myles <mylesssalazar@gmail.com>
2020-04-23 03:59:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function decrement(n){
 function makeInt(n){
   return parseInt(n,10)
 }
-function perserveDecimal(n){
-  return parseFloat(n, 80.123999)
+function preserveDecimal(n){
+  return parseFloat(n)
 }
